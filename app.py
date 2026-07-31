684f2df2952ec695509dc55727616310d46c090c
Hoangkill <jj5s9pndpn@privaterelay.appleid.com>
2026-07-31 10:53:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -424,3 +424,10 @@ def get_account_info():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    @app.route("/")
+def home():
+    return {
+        "status": "online",
+        "message": "FreeFire API Running",
+        "version": "1.0"
+    }
